e23938ef9169027e4f28408be82d7b5735d9b0b6
watulion <rdo_cnt@hotmail.com>
2016-04-21 15:41:36 -0500

    data between sessions and scenes

---
+++ Proyecto final apis/Assets/Scripts/LevelNumManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic; 
+
+public class LevelNumManager : MonoBehaviour {
+
+	string levelNum;
+	Text theText;
+
+	// Use this for initialization
+	void Start () {
+		theText = GetComponent<Text> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		theText.text = "" + PlayerPrefs.GetInt ("Level");
+	}
+}
---
+++ Proyecto final apis/Assets/Scripts/mainMenuManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;	
+using System.Collections;
+using System.Collections.Generic; 
+using System.Runtime.Serialization.Formatters.Binary; 
+using System.IO;
+
+public class mainMenuManager : MonoBehaviour {
+
+	SceneManager manageScene;
+	string easyScene;
+	int level = 0;
+
+
+
+	// Use this for initialization
+	void Start () {
+		//save data
+		easyScene = "EasyGame";
+		if (PlayerPrefs.GetInt ("Level") < 1) {
+			PlayerPrefs.SetInt ("Level", 1);
+			PlayerPrefs.Save ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Easy(){
+		SceneManager.LoadScene(easyScene);
+		PlayerPrefs.SetInt ("Level", PlayerPrefs.GetInt ("Level") + 1);
+	}
+}
