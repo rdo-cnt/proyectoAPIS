d71ed876cbd6a15f33bdaa3a82666c6ef80f6069
watulion <rdo_cnt@hotmail.com>
2016-04-23 14:19:49 -0500

    Camera lighting fixed ratio

---
+++ Proyecto final apis/Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Camera.main.aspect = 16f / 9f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
