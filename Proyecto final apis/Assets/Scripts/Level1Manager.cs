72293ac927bc5db323eb5bc710fc42dafeb80c3e
watulion <rdo_cnt@hotmail.com>
2016-04-25 17:20:26 -0500

    Nivel1inicializado

---
+++ Proyecto final apis/Assets/Scripts/Level1Manager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level1Manager : MonoBehaviour {
+
+	public GameObject[] labels;
+	public int playerHealth;
+	public int enemyHealth;
+	public string[] preguntas;
+	public string[] respuestas;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Repartir(){
+		int randomNum = Random.Range (0, 3);
+		int randomNumPregunta = Random.Range (0, 34);
+	}
+
+}
---
+++ Proyecto final apis/Assets/Scripts/labelManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class labelManager : MonoBehaviour {
+
+	public GameObject LevelManager;
+	public Text myText;
+	public int myID;
+
+	// Use this for initialization
+	void Start () {
+		myText = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
