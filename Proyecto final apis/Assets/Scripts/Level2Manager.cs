931e4b43aa37a69041e7d633ae769c1768494b65
watulion <rdo_cnt@hotmail.com>
2016-04-28 16:48:47 -0500

    nivel2 con imagenes

---
+++ Proyecto final apis/Assets/Scripts/Level2Manager.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Level2Manager : MonoBehaviour {
+
+	public labelManager[] labels;
+	public int playerHealth;
+	public int enemyHealth;
+
+	public pregunta2Manager preguntaImage;
+	public HealthManager HP;
+
+	int conta = 0;
+	public int randomNum;
+	public int randomNumPregunta;
+	public string[] respuestasCorrectas = new string[35] {"H", "He", "Li","Be", "B", "C","N", "O", "F","Ne", "Na", "Mg","Al", "Si", "P","S", "Cl", "Ar","K", "Ca", "Sc","Ti", "V", "Cr","Mn", "Fe", "Co","Ni", "Cu", "Zn","Ga", "Ge", "As","Se", "Br"};
+	public Sprite[] preguntas;
+	public string[] respuestaIncorrectas1 = new string[35] {"Hi", "H", "L","B", "Be", "Ca","Ni", "Ox", "Fl","N", "S", "Ma","A", "S", "F","A", "C", "Arg","Po", "Cal", "Es","T", "Va", "C","Man", "Hi", "C","N", "C", "Zi","G", "G", "Ar","S", "Bro"};
+	public string[] respuestaIncorrectas2 = new string[35] {"Hid", "Hel", "Lit","Ber", "Bor", "Car","Nit", "Oxi", "Flu","Neo", "Sod", "Mag","Alu", "Sil", "Fos","Asu", "Clo", "A","Pot", "C", "Esc","Tit", "Van", "Cro","Ma", "Hie", "Cob","Niq", "Cob", "Zin","Gal", "Ger", "Ars","Sel", "B"};
+	public string[] respuestaIncorrectas3 = new string[35] {"Hd", "Hl", "Lt","Br", "Br", "Cr","Nt", "Oi", "Fu","No", "Sd", "Mgn","Au", "Sl", "Ps","Au", "Co", "Ag","Ka", "Cl", "Sca","Tt", "Vn", "Co","Mg", "He", "Cb","Nq", "Cb", "Cn","Gl", "Gr", "A","Sl", "Bo"};
+
+	void Start () {
+		
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (conta == 0) {
+			Repartir ();
+			conta++;
+		}	
+
+		HP.HP.text = playerHealth.ToString();
+
+		//primero checar vida
+		if (playerHealth == 0) {
+			SceneManager.LoadScene ("EasyGame");
+		}
+
+		if (enemyHealth == 0) {
+			SceneManager.LoadScene ("EasyGame");
+			PlayerPrefs.SetInt ("Lvl3", 1);
+		}
+	}
+
+	public void Repartir(){
+		randomNum = Random.Range (0, 4);
+		randomNumPregunta = Random.Range (0, 35);
+		preguntaImage.pregunta.sprite = preguntas [randomNumPregunta];
+
+
+		labels[randomNum].myID = randomNumPregunta;
+		labels[randomNum].myText.text = respuestasCorrectas[randomNumPregunta];
+
+		int contador = 0;
+		for (int i = 0, iRandom = Random.Range(0,4) ; i < 4; i++, iRandom++) {
+			if (iRandom % 4 != randomNum) {
+				switch (i) {
+				case 0:
+					labels [iRandom % 4].myID = -1;
+					labels [iRandom % 4].myText.text = respuestaIncorrectas1 [randomNumPregunta];
+					break;
+				case 1:
+					labels [iRandom % 4].myID = -1;
+					labels [iRandom % 4].myText.text = respuestaIncorrectas2 [randomNumPregunta];
+					break;
+				case 2:
+					labels [iRandom % 4].myID = -1;
+					labels [iRandom % 4].myText.text = respuestaIncorrectas3 [randomNumPregunta];
+					break;
+				}
+			} else {
+				i--;
+			}
+		}
+
+	}
+
+	public void checkCorrect(int id){
+		if (randomNumPregunta == id) {
+			enemyHealth--;
+			Repartir ();
+		} else {
+			playerHealth--;
+		}
+	}
+
+}
---
+++ Proyecto final apis/Assets/Scripts/pregunta2Manager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class pregunta2Manager : MonoBehaviour {
+
+	public SpriteRenderer pregunta;
+
+
+	// Use this for initialization
+	void Start () {
+		pregunta = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
